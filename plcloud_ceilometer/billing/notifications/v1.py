b3cf5ea9f816adfaa0733e5688b94aea742ffc0f
zhennan.luo <zhennan.luo@powerleader.com.cn>
2018-03-07 17:24:22 +0800

    fix code

--- plcloud_ceilometer/billing/notifications/v1.py
+++ plcloud_ceilometer/billing/notifications/v1.py
@@ -5,6 +5,7 @@ __author__ = "Jenner.luo"
 
 import oslo_messaging
 from oslo_log import log
+from ceilometer.i18n import _
 from .base import BillingBase
 
 LOG = log.getLogger(__name__)
@@ -16,11 +17,11 @@ class Instance(BillingBase):
     def get_targets(self, conf):
         """oslo.messaging.TargetS for this plugin."""
         return [oslo_messaging.Target(topic=topic,
-                                 exchange=conf.nova_control_exchange)
+                                      exchange=conf.nova_control_exchange)
                 for topic in self.get_notification_topics(conf)]
 
     def process_notification(self, message):
-        LOG.debug(_('Instance notification %r') % message)
+        print 'Instance notification=', message
         LOG.debug(_('Instance notification %r') % message)
         user_id = message['payload']['user_id']
         tenant_id = message['payload']['tenant_id']
@@ -34,6 +35,42 @@ class Instance(BillingBase):
                     'vcpus': message['payload']['vcpus'],
                     'disk_gb': message['payload']['disk_gb'],
                     'ephemeral_gb': message['payload']['ephemeral_gb']}
+        return {'message_id': message_id,
+                'res_id': res_id,
+                'res_name': res_name,
+                'res_meta': res_meta,
+                'res_type': res_type,
+                'event_type': event_type,
+                'timestamp': timestamp,
+                'user_id': user_id,
+                'tenant_id': tenant_id}
+
+
+class Volume(BillingBase):
+    event_types = [
+        "volume.create.end",
+        "volume.delete.end",
+        "volume.resize.end",
+    ]
+
+    def get_targets(self, conf):
+        """oslo.messaging.TargetS for this plugin."""
+        return [oslo_messaging.Target(topic=topic,
+                                      exchange=conf.cinder_control_exchange)
+                for topic in self.get_notification_topics(conf)]
+
+    def process_notification(self, message):
+        print 'Volume notification=', message
+        LOG.debug(_('Volume notification %r') % message)
+        user_id = message['payload']['user_id']
+        tenant_id = message['payload']['tenant_id']
+        res_id = message['payload']['volume_id']
+        res_name = message['payload']['display_name']
+        res_type = 'volume'
+        message_id = message['message_id']
+        timestamp = message['timestamp']
+        event_type = message['event_type']
+        res_meta = {'volume_gb': message['payload']['size']}
         return {'message_id': message_id,
                 'res_id': res_id,
                 'res_name': res_name,
