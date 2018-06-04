057461cde5394ab62fed2b86f1e743eb06b2698e
zhennan.luo <zhennan.luo@powerleader.com.cn>
2018-06-04 13:45:30 +0800

    fix code

--- plcloud_ceilometer/tests/unit/fakes.py
+++ plcloud_ceilometer/tests/unit/fakes.py
@@ -4,11 +4,11 @@
 __author__ = "Jenner.luo"
 
 
-def plck_client_get_endpoint(self, *args, **kwargs):
+def plck_client_get_endpoint(*args, **kwargs):
     return "http://192.168.215.38:8899"
 
 
-def keystone_client_get_session():
+def keystone_client_get_session(*args, **kwargs):
     class Session(object):
         def get_token(self):
             return "4386a57966ea4ad79b48532f50812717"
