b3025bd742191212c0b769b051e927b4eb8ff2a8
zhennan.luo <zhennan.luo@powerleader.com.cn>
2018-06-04 11:28:21 +0800

    Fix code

--- plcloud_ceilometer/tests/unit/test_plcloudkittyclient.py
+++ plcloud_ceilometer/tests/unit/test_plcloudkittyclient.py
@@ -8,6 +8,7 @@ from oslotest import base
 from oslo_config import fixture as fixture_config
 from ceilometer import service
 from plcloud_ceilometer.clients.plcloudkitty import PLCloudkittyClient
+from plcloudkittyclient import client as plck_client
 
 
 class TestPLClient(base.BaseTestCase):
@@ -15,7 +16,13 @@ class TestPLClient(base.BaseTestCase):
         super(TestPLClient, self).setUp()
         conf = service.prepare_service([], [])
         self.CONF = self.useFixture(fixture_config.Config(conf)).conf
+        self.useFixture(mockpatch.PatchObject(
+            plck_client, '_get_endpoint',
+            side_effect=self.plck_client_get_endpoint))
         self.plclient = PLCloudkittyClient(self.CONF)
+
+    def plck_client_get_endpoint(self, *args, **kwargs):
+        return "http://192.168.215.38:8899"
 
     @classmethod
     def fake_data_from_get_billing(cls):
