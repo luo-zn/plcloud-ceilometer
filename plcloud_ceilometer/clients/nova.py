eacf2dd4b7aaf1dc460fc4d360b2a12d0252d15a
zhennan.luo <zhennan.luo@powerleader.com.cn>
2018-06-05 18:00:00 +0800

    Fix initialize_client_hook

--- plcloud_ceilometer/clients/nova.py
+++ plcloud_ceilometer/clients/nova.py
@@ -5,8 +5,11 @@ __author__ = "Jenner.luo"
 
 from oslo_log import log
 from oslo_config import cfg
+from novaclient import api_versions
+from novaclient import client as nova_client
+from ceilometer import keystone_client
 from . import ClientBase
-from ceilometer import nova_client
+
 
 LOG = log.getLogger(__name__)
 
@@ -17,7 +20,20 @@ class NovaClient(ClientBase):
 
     def initialize_client_hook(self):
         """Initialize a Nova client object."""
-        return nova_client.Client(self.conf)
+        creds = conf.service_credentials
+        logger = None
+        if conf.nova_http_log_debug:
+            logger = log.getLogger("novaclient-debug")
+            logger.logger.setLevel(log.DEBUG)
+        ks_session = keystone_client.get_session(conf)
+        return nova_client.Client(
+            version=api_versions.APIVersion('2.1'),
+            session=ks_session,
+            # nova adapter options
+            region_name=creds.region_name,
+            endpoint_type=creds.interface,
+            service_type=conf.service_types.nova,
+            logger=logger)
 
     def create_image(self, instance_id, name):
         self.client.servers.create_image(instance_id, name)
