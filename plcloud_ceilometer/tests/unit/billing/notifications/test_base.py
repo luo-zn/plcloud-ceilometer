a22367493cde448f4289979cc3b9a28bad18dfff
zhennan.luo <zhennan.luo@powerleader.com.cn>
2018-06-05 11:29:10 +0800

    fix code

--- plcloud_ceilometer/tests/unit/billing/notifications/test_base.py
+++ plcloud_ceilometer/tests/unit/billing/notifications/test_base.py
@@ -7,6 +7,7 @@ import mock
 from oslotest import base
 from oslo_config import fixture as fixture_config, cfg
 from plcloud_ceilometer.billing.notifications import base as plugin_base
+from plcloud_ceilometer.tests.unit import fakes
 
 
 class TestBillingBase(base.BaseTestCase):
@@ -36,6 +37,10 @@ class TestBillingBase(base.BaseTestCase):
         def plcloudkitty_billing(self, notification):
             pass
 
+    @mock.patch('plcloudkittyclient.client._get_endpoint',
+                fakes.plck_client_get_endpoint)
+    @mock.patch('ceilometer.keystone_client.get_session',
+                fakes.keystone_client_get_session)
     def test_plugin_info(self):
         plugin = self.FakeBillingBase(self.fake_manager)
         plugin.to_samples_and_publish = mock.Mock()
