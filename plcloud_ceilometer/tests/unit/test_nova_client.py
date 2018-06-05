5acc0a6a606fc003461f4f70d84be956405f805e
zhennan.luo <zhennan.luo@powerleader.com.cn>
2018-06-05 19:50:21 +0800

    Fix code

--- plcloud_ceilometer/tests/unit/test_nova_client.py
+++ plcloud_ceilometer/tests/unit/test_nova_client.py
@@ -18,13 +18,25 @@ class TestNovaClient(base.BaseTestCase):
         self.nvc = NovaClient(self.CONF)
 
     @classmethod
-    def fake_servers_list(cls,*args, **kwargs):
+    def fake_servers_list(cls, *args, **kwargs):
         a = mock.MagicMock()
         a.id = 42
+        a.name = "host-{}".format(b.id)
+        a.user_id = '914947b388194de8ad1e9e2f7123bb78'
+        a.addresses = {
+            'demo-net': [{'OS-EXT-IPS-MAC:mac_addr': 'fa:16:3e:b8:b3:50',
+                          'version': 4, 'addr': '10.0.0.9',
+                          'OS-EXT-IPS:type': 'fixed'}]}
         a.flavor = {'id': 1}
         a.image = {'id': 1}
         b = mock.MagicMock()
         b.id = 43
+        b.name = "host-{}".format(b.id)
+        b.user_id = '914947b487184de8ad1e9e2f7123bb78'
+        b.addresses = {'demo-net': [{'OS-EXT-IPS-MAC:mac_addr':
+                                         'fa:15:4e:b8:b3:51',
+                                     'version': 4, 'addr': '10.0.0.10',
+                                     'OS-EXT-IPS:type': 'fixed'}]}
         b.flavor = {'id': 2}
         b.image = {'id': 2}
         return [a, b]
