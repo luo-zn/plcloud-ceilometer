6437c8e31b00cb86dfafcde901fa46ae8a1c1031
zhennan.luo <zhennan.luo@powerleader.com.cn>
2018-06-06 14:25:02 +0800

    add code

---
+++ plcloud_ceilometer/tests/unit/test_utils.py
@@ -0,0 +1,23 @@
+"""
+# Author: Zhennan.luo(Jenner)
+"""
+__author__ = "Jenner.luo"
+
+import mock
+import unittest
+from plcloud_ceilometer import utils
+
+
+class TestUtils(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test_catch_log(self):
+        mo = mock.Mock()
+
+        @utils.catch_log
+        def mock_func():
+            raise Exception('Mock Exception!')
+        mo.method_with_catch_log = mock_func
+
+        mo.method_with_catch_log()
