dfe9fbf643f5b590634e66cfe17955c65b8072de
zhennan.luo <zhennan.luo@powerleader.com.cn>
2018-06-06 18:16:48 +0800

    add test

--- plcloud_ceilometer/tests/unit/clients/test_cinder.py
+++ plcloud_ceilometer/tests/unit/clients/test_cinder.py
@@ -33,5 +33,44 @@ class TestCinderClient(base.BaseTestCase):
             help='Cinder service type.'),
         ], group="service_types")
 
+    @classmethod
+    def fake_volume(cls, *args, **kwargs):
+        a = {u'migration_status': None, u'attachments': [],
+             u'availability_zone': u'nova',
+             u'os-vol-host-attr:host': u'all-in-one@lvm-1#lvm-1',
+             u'encrypted': False, u'updated_at': u'2018-05-30T12:18:13.000000',
+             u'replication_status': None, u'snapshot_id': None,
+             u'id': u'9bbc9fec-79cb-469d-adb9-ff19b4c84117', u'size': 1,
+             u'user_id': u'914947b388194de8ad1e9e2f7123bb78',
+             u'os-vol-tenant-attr:tenant_id': u'e354e01da1a54aab9b471b77422549e6',
+             u'os-vol-mig-status-attr:migstat': None, u'metadata': {},
+             u'status': u'available', u'description': u'',
+             u'multiattach': False, u'source_volid': None,
+             u'consistencygroup_id': None,
+             u'os-vol-mig-status-attr:name_id': None, u'name': u'test-volume',
+             u'bootable': u'false',
+             u'created_at': u'2018-05-30T12:18:13.000000',
+             u'volume_type': None}
+        return mock.MagicMock(**a)
+
+    @classmethod
+    def fake_volumes_list(cls, *args, **kwargs):
+        return [cls.fake_volume()]
+
+    def test_get_volume(self):
+        with mock.patch.object(self.cc.client.volumes, 'get',
+                               self.fake_volume):
+            volume = self.cc.get_volume('9bbc9fec-79cb-469d-adb9-ff19b4c84117')
+        self.assertEqual('test-volume', volume.get('test-volume'))
+        for key in ['name', 'id', 'status', 'availability_zones',
+                    'volume_type']:
+            self.assertIn(key, volume)
+
     def test_get_all_volume(self):
+        with mock.patch.object(self.cc.client.volumes, 'list',
+                               side_effect=self.fake_volumes_list):
+            volumes = self.cc.get_all_volume()
+        self.assertEqual(1, len(volumes))
+        self.assertEqual('9bbc9fec-79cb-469d-adb9-ff19b4c84117',
+                         volumes[0]['id'])
         print dir(self.cc)
