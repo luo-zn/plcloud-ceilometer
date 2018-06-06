a69dfb89250dcf67165b7a5275cd163f33ccd47c
zhennan.luo <zhennan.luo@powerleader.com.cn>
2018-06-06 16:41:26 +0800

    add code

--- plcloud_ceilometer/clients/cinder.py
+++ plcloud_ceilometer/clients/cinder.py
@@ -5,9 +5,19 @@ __author__ = "Jenner.luo"
 
 from oslo_log import log
 from oslo_config import cfg
-from . import ClientBase
+from ceilometer import keystone_client
+from . import ClientBase, APIVersionManager
 
 LOG = log.getLogger(__name__)
+
+VERSIONS = APIVersionManager("volume", preferred_version=2)
+try:
+    from cinderclient.v2 import client as cinder_client_v2
+
+    VERSIONS.load_supported_version(2, {"client": cinder_client_v2,
+                                        "version": 2})
+except ImportError:
+    pass
 
 
 class CinderClient(ClientBase):
@@ -17,4 +27,12 @@ class CinderClient(ClientBase):
     def initialize_client_hook(self):
         """Initialize a Cinder client object."""
         # return cinder_client.Client(self.conf)
-        return None
+        api_version = VERSIONS.get_active_version()
+        creds = self.conf.service_credentials
+        params = {
+            'session': keystone_client.get_session(self.conf),
+            'endpoint_type': creds.interface,
+            'region_name': creds.region_name,
+            'service_type': self.conf.service_types.cinder,
+        }
+        return api_version['client'].Client(**params)
